23fbd7669eb96dcb1a68ad2c19ecdbabe5ab22b8
Roshan Davis <75558109+RoshanDavis@users.noreply.github.com>
2023-04-30 16:21:22 +0530

    create home page

---
+++ frontend/src/Components/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navbar from './Navbar'
+import './Home.css'
+const Home = () => {
+  return (
+    <div>
+        <nav>
+            <Navbar/>
+        </nav>
+        <body className='home-body'>
+            <div className='text-container'>
+                <h1>
+                    Order your favorite food with us.
+                </h1>
+                <div>
+                    <h2>Restaurants in your pocket</h2>
+                    <p>Order from your favorite restaurants with the all-new Foodiko app.</p>
+                </div>
+            </div>
+            <img src={require('../Assets/Home-Page-Food.png')} alt="" />
+        </body>
+    </div>
+  )
+}
+
+export default Home
