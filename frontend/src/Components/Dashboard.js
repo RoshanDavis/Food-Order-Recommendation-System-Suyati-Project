82ac872793336b02213164b5e99c01ec5937dbfd
Roshan Davis <75558109+RoshanDavis@users.noreply.github.com>
2023-05-04 14:11:22 +0530

    Added background image and text

--- frontend/src/Components/Dashboard.js
+++ frontend/src/Components/Dashboard.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import Navbar2 from './Navbar2'
 import Footer from './Footer'
+import './Dashboard.css'
 const Dashboard = () => {
   return (
-    <div>
+    <div className='dashboard'>
         <nav>
             <Navbar2/>
         </nav>
-        <body>
-            
+        <body className='dashboard-body'>
+            <div className="bg-img position-relative d-flex justify-content-center">
+              <h1 className>Satisfy your cravings</h1> 
+            </div>
         </body>
         <footer>
             <Footer/>
