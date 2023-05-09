db96fe544a9f36bdbfc361133afe80382f7a10e2
Roshan Davis <75558109+RoshanDavis@users.noreply.github.com>
2023-05-09 23:23:02 +0530

    Food item rating bugfix

--- frontend/src/Components/FoodItem.jsx
+++ frontend/src/Components/FoodItem.jsx
@@ -42,13 +42,17 @@ const FoodItem = ({data,showItemCountProp}) => {
                 </div>
                 <div className="card-bottom">
                 <h1 className='card-bottom-restuarant-name'>{item.restaurant}</h1>
+                
                 <div className="rating ">
+                    {item.rating &&
                     <div className="rating-bg-color  d-flex flex-row rounded ps-2 pe-2 ">
                     <h1 className={`${ item.rating && item.rating.toString().length === 1 ? 'rating--large' : 'rating--small'}`}>{item.rating}</h1> 
                     
                     <img className='pt-1 ps-1' src={require('../Assets/Star.png')} alt="" />
                     </div>
+                    }
                 </div>
+            
                 <h3>{item.food}</h3>
                 <h2>Rs.{item.price}</h2>
                 </div>
--- frontend/src/Components/ProductSlider.js
+++ frontend/src/Components/ProductSlider.js
@@ -1,10 +1,9 @@
 
-import React,{useState} from 'react'
+import React from 'react'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import './ProductSlider.css';
-import imgGirl from '../Assets/Icon.png';
 import { Link } from 'react-router-dom';
 import FoodItem from './FoodItem';
 
@@ -32,18 +31,7 @@ function CustomNextArrow(props) {
 
 const ProductSlider = ({ data,showItemCountProp }) => {
 
-  // const [count, setCount] = useState(0);
-  
-  //   const handleIncrement = () => {
-  //       setCount(count + 1);
-  //   };
-    
-  //   const handleDecrement = () => {
-  //       if(count>0)
-  //       setCount(count - 1);
-  //   };
 
-    const [defaultImage, setDefaultImage] = useState({});
   const settings = {
     dots: true,
     infinite: false,
@@ -82,13 +70,7 @@ const ProductSlider = ({ data,showItemCountProp }) => {
     ],
   };
 
-  const handleErrorImage = (data) => {
-    setDefaultImage((prev) => ({
-      ...prev,
-      [data.target.alt]: data.target.alt,
-      linkDefault: imgGirl,
-    }));
-  };
+  
 
  
 
@@ -97,42 +79,7 @@ const ProductSlider = ({ data,showItemCountProp }) => {
       <Slider {...settings}>
         {data.map((item) => (
           <Link to="" className='no-text-decoration'>
-            {/* <div className="card-top">
-              <img
-                src={
-                  defaultImage[item.restaurant] === item.restaurant
-                    ? defaultImage[item.restaurant] === defaultImage["'" + item.restaurant + "'"]? defaultImage.linkDefault: item.linkImg
-                    : item.linkImg
-                }
-                alt={item.restaurant}
-                onError={handleErrorImage}
-              />
-                
-            </div>
-            <div className="card-bottom">
-              <h1 className='card-bottom-restuarant-name'>{item.restaurant}</h1>
-              <div className="rating ">
-                {item.rating &&
-                <div className="rating-bg-color  d-flex flex-row rounded ps-2 pe-2 ">
-                  <h1 className={`${ item.rating && item.rating.toString().length === 1 ? 'rating--large' : 'rating--small'}`}>{item.rating}</h1> 
-                  
-                  <img className='pt-1 ps-1' src={require('../Assets/Star.png')} alt="" />
-                </div>
-                }
-              </div>
-              <h3>{item.food}</h3>
-              <h2>Rs.{item.price}</h2>
-            </div>
-            { showItemCountProp &&
-                    <div className="item-count d-flex flex-row justify-content-around pt-3 pb-3" style={{fontSize: '3rem'}}>
-
-                        <div onClick={handleDecrement} class="btn  restaurant-btn add-sub-btn p-0" >-</div>
-                        <div style={{fontSize: '2rem', color: 'grey'}}>{count}</div>
-                        <div onClick={handleIncrement} class="btn  restaurant-btn add-sub-btn p-0">+</div>
-                    </div>
-            } */}
             <FoodItem data={[item]} showItemCountProp={showItemCountProp}/>
-            {console.log(item)}
           </Link>
         ))}
       </Slider>
